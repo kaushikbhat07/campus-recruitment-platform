8cca82bd0bb67529c6781b9463dca1572180dd74
KB086364 <kaushik.bhat11@learner.manipal.edu>
2021-12-06 01:54:34 +0530

    PAge not found

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-// import logo from "./logo.svg";
-import { Route } from "react-router-dom";
-import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./HomePage";
 import About from "./About";
+import PageNotFound from "./PageNotFound";
 
 function App() {
 	return (
 		<div className="App">
-			{/* <header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Hey this works
-				</a>
-			</header> */}
-			<Route exact path="/" component={HomePage} />
-			<Route path="/about" component={About} />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/about" component={About} />
+				<Route component={PageNotFound} />
+			</Switch>
 		</div>
 	);
 }
