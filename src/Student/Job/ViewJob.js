e0fcf7b78e972a5439a6eb79fb9dd0422283d876
KB086364 <kaushik.bhat11@learner.manipal.edu>
2021-12-13 00:55:36 +0530

    View jobs UI changes - added accordion

--- src/Student/Job/ViewJob.js
+++ src/Student/Job/ViewJob.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
+// import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
-import Form from "react-bootstrap/Form";
-import Col from "react-bootstrap/Col";
+// import Form from "react-bootstrap/Form";
+// import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Accordion from "react-bootstrap/Accordion";
 
@@ -16,6 +16,38 @@ class ViewJob extends React.Component {
             <Row>
                 <div>
                     <h5 className="mb-4">Job Openings</h5>
+                    <Accordion defaultActiveKey="0" className="mb-4 shadow">
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>
+                                <h5>Cerner</h5>
+                            </Accordion.Header>
+                            <Accordion.Body>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint
+                                occaecat cupidatat non proident, sunt in culpa
+                                qui officia deserunt mollit anim id est laborum.
+                                <div class="text-muted text-end">
+                                    2 days ago
+                                </div>
+                                <hr />
+                                <div className="text-end">
+                                    <Button variant="danger">
+                                        Apply now{" "}
+                                        <i class="bi bi-pencil-square"></i>
+                                    </Button>
+                                </div>
+                            </Accordion.Body>
+                            {/* <div class="text-muted text-end card-footer">
+                                2 days ago
+                            </div> */}
+                        </Accordion.Item>
+                    </Accordion>
                     <Accordion defaultActiveKey="0" className="mb-4 shadow">
                         <Accordion.Item eventKey="0">
                             <Accordion.Header>
@@ -38,7 +70,8 @@ class ViewJob extends React.Component {
                                 <hr />
                                 <div className="text-end">
                                     <Button variant="danger">
-                                        Apply <i class="bi bi-check2"></i>
+                                        Apply now{" "}
+                                        <i class="bi bi-pencil-square"></i>
                                     </Button>
                                 </div>
                             </Accordion.Body>
@@ -64,85 +97,55 @@ class ViewJob extends React.Component {
                                 dolore eu fugiat nulla pariatur. Excepteur sint
                                 occaecat cupidatat non proident, sunt in culpa
                                 qui officia deserunt mollit anim id est laborum.
+                                <div class="text-muted text-end">
+                                    2 days ago
+                                </div>
+                                <hr />
+                                <div className="text-end">
+                                    <Button variant="danger">
+                                        Apply now{" "}
+                                        <i class="bi bi-pencil-square"></i>
+                                    </Button>
+                                </div>
                             </Accordion.Body>
+                            {/* <div class="text-muted text-end card-footer">
+                                2 days ago
+                            </div> */}
                         </Accordion.Item>
                     </Accordion>
 
-                    <Card className="shadow mb-4">
-                        <Card.Body>
-                            <div>
-                                <h5>Amazon</h5>
-                            </div>
-                            Nostrud sint ea sit anim id mollit cupidatat. Velit
-                            nostrud commodo non amet culpa labore deserunt
-                            fugiat mollit minim. Officia ex commodo tempor enim
-                            tempor enim pariatur irure culpa labore. Quis
-                            incididunt quis amet incididunt cupidatat mollit
-                            commodo. Qui enim ut reprehenderit aliqua id est.
-                            Sunt eiusmod excepteur ipsum adipisicing dolore
-                            Lorem voluptate cupidatat deserunt velit laborum
-                            aliqua dolore et. Occaecat nostrud sint esse aliqua
-                            consequat nulla. Nulla ipsum ad non consectetur est
-                            duis sit tempor irure eu. Deserunt labore laborum ea
-                            ex officia magna ad ad consequat consequat proident.
-                            <div className="text-end">
-                                <Button variant="danger">
-                                    Apply <i class="bi bi-check2"></i>
-                                </Button>
-                            </div>
-                        </Card.Body>
-                        <Card.Footer className="text-muted text-end">
-                            2 days ago
-                        </Card.Footer>
-                    </Card>
-                    <Card className="shadow mb-4">
-                        <Card.Body>
-                            Nostrud sint ea sit anim id mollit cupidatat. Velit
-                            nostrud commodo non amet culpa labore deserunt
-                            fugiat mollit minim. Officia ex commodo tempor enim
-                            tempor enim pariatur irure culpa labore. Quis
-                            incididunt quis amet incididunt cupidatat mollit
-                            commodo. Qui enim ut reprehenderit aliqua id est.
-                            Sunt eiusmod excepteur ipsum adipisicing dolore
-                            Lorem voluptate cupidatat deserunt velit laborum
-                            aliqua dolore et. Occaecat nostrud sint esse aliqua
-                            consequat nulla. Nulla ipsum ad non consectetur est
-                            duis sit tempor irure eu. Deserunt labore laborum ea
-                            ex officia magna ad ad consequat consequat proident.
-                            <div className="text-end">
-                                <Button variant="danger">
-                                    Apply <i class="bi bi-check2"></i>
-                                </Button>
-                            </div>
-                        </Card.Body>
-                        <Card.Footer className="text-muted text-end">
-                            2 days ago
-                        </Card.Footer>
-                    </Card>
-                    <Card className="shadow mb-4">
-                        <Card.Body>
-                            Nostrud sint ea sit anim id mollit cupidatat. Velit
-                            nostrud commodo non amet culpa labore deserunt
-                            fugiat mollit minim. Officia ex commodo tempor enim
-                            tempor enim pariatur irure culpa labore. Quis
-                            incididunt quis amet incididunt cupidatat mollit
-                            commodo. Qui enim ut reprehenderit aliqua id est.
-                            Sunt eiusmod excepteur ipsum adipisicing dolore
-                            Lorem voluptate cupidatat deserunt velit laborum
-                            aliqua dolore et. Occaecat nostrud sint esse aliqua
-                            consequat nulla. Nulla ipsum ad non consectetur est
-                            duis sit tempor irure eu. Deserunt labore laborum ea
-                            ex officia magna ad ad consequat consequat proident.
-                            <div className="text-end">
-                                <Button variant="danger">
-                                    Apply <i class="bi bi-check2"></i>
-                                </Button>
-                            </div>
-                        </Card.Body>
-                        <Card.Footer className="text-muted text-end">
-                            2 days ago
-                        </Card.Footer>
-                    </Card>
+                    <Accordion defaultActiveKey="0" className="mb-4 shadow">
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>
+                                <h5>Uber</h5>
+                            </Accordion.Header>
+                            <Accordion.Body>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint
+                                occaecat cupidatat non proident, sunt in culpa
+                                qui officia deserunt mollit anim id est laborum.
+                                <div class="text-muted text-end">
+                                    2 days ago
+                                </div>
+                                <hr />
+                                <div className="text-end">
+                                    <Button variant="danger">
+                                        Apply now{" "}
+                                        <i class="bi bi-pencil-square"></i>
+                                    </Button>
+                                </div>
+                            </Accordion.Body>
+                            {/* <div class="text-muted text-end card-footer">
+                                2 days ago
+                            </div> */}
+                        </Accordion.Item>
+                    </Accordion>
                 </div>
             </Row>
         );
